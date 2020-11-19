cd4f0c3fae826842c1e5d43d4a2f2cc118a0aa64
carlosayol <Carlosaolar@gmail.com>
2020-11-19 11:59:59 -0500

    optimizando la pagina home (falta correr en cmd)

--- pages/home/login_page.py
+++ pages/home/login_page.py
@@ -5,15 +5,41 @@ class LoginPage():
     def __init__(self, driver):
         self.driver = driver
 
+    #Locators
+
+    _signin_link = "//a[contains(text(),'Sign In')]"
+    _email_field = "email"
+    _password_field = "password"
+    _login_button = "//input[@value='Login']"
+
+    def getSignInLink(self):
+        return self.driver.find_element(By.XPATH,self._signin_link)
+
+    def getEmailField(self):
+        return self.driver.find_element(By.ID,self._email_field)
+
+    def getPasswordField(self):
+        return self.driver.find_element(By.ID,self._password_field)
+
+    def getLoginButton(self):
+        return self.driver.find_element(By.XPATH,self._login_button)
+
+    #Acciones
+
+    def clickLoginLink(self):
+        self.getSignInLink().click()
+
+    def enterEmail(self, email):
+        self.getEmailField().send_keys(email)
+
+    def enterPassword(self, password):
+        self.getPasswordField().send_keys(password)
+
+    def clickLoginButton(self):
+        self.getLoginButton().click()
+
     def login(self, username="test@email.com", password="abcabc"):
-        loginLink = self.driver.find_element(By.XPATH,"//a[contains(text(),'Sign In')]")
-        loginLink.click()
-
-        emailForm = self.driver.find_element(By.ID,"email")
-        emailForm.send_keys(username)
-
-        passForm = self.driver.find_element(By.ID,"password")
-        passForm.send_keys(password)
-
-        loginButton = self.driver.find_element(By.XPATH,"//input[@value='Login']")
-        loginButton.click()
+        self.clickLoginLink()
+        self.enterEmail(username)
+        self.enterPassword(password)
+        self.clickLoginButton()
