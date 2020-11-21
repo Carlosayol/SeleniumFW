00fba06789e9126c51708e4b1d9e3becf0dbf12c
carlosayol <Carlosaolar@gmail.com>
2020-11-21 10:13:55 -0500

    added custom logger

--- base/selenium_driver.py
+++ base/selenium_driver.py
@@ -3,8 +3,13 @@ from traceback import print_stack
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import *
+import logging
+import utilities.customlogger as cl
+
 
 class SeleniumDriver():
+
+    log = cl.customLogger(logLevel=logging.DEBUG)
 
     def __init__(self, driver):
         self.driver = driver
@@ -24,7 +29,7 @@ class SeleniumDriver():
         elif locatorType == "name":
             return By.NAME
         else:
-            print("Locator type is not supported")
+            self.log.info("Locator type is not supported")
         return False
 
     def getElement(self, locator, locatorType="id"):
@@ -33,49 +38,49 @@ class SeleniumDriver():
             locatorType = locatorType.lower()
             byType = self.getByType(locatorType)
             element = self.driver.find_element(byType, locator)
-            print("Element found")
+            self.log.info("Element found")
         except:
-            print("Element not found")
+            self.log.info("Element not found")
         return element
 
     def elementClick(self, locator, locatorType="id"):
         try:
             element = self.getElement(locator, locatorType)
             element.click()
-            print("Element has been clicked")
+            self.log.info("Element has been clicked")
         except:
-            print("Cannot click the element")
+            self.log.info("Cannot click the element")
             print_stack()
 
     def sendElementKeys(self, locator, data,locatorType="id"):
         try:
             element = self.getElement(locator, locatorType)
             element.send_keys(data)
-            print("Keys has been send")
+            self.log.info("Keys has been send")
         except:
-            print("Cannot send keys")
+            self.log.info("Cannot send keys")
             print_stack()
 
     def isElementPresent(self, locator, byType):
         try:
             element = self.driver.find_element(byType, locator)
             if element is not None:
-                print("Element found")
+                self.log.info("Element found")
                 return True
             else: return False
         except:
-            print("Element not found")
+            self.log.info("Element not found")
             return False
 
     def ElementsPresent(self, locator, byType):
         try:
             elements = self.driver.find_elements(byType, locator)
             if len(elements)>0:
-                print("Element found")
+                self.log.info("Element found")
                 return True
             else: return False
         except:
-            print("Element not found")
+            self.log.info("Element not found")
             return False
 
     def waitForElement(self, locator, locatorType="id",timeout=10,
@@ -84,14 +89,14 @@ class SeleniumDriver():
         element = None
         try:
             byType = self.hw.getByType(locatorType)
-            print("Esperando a :: " +str(timeout)+ " :: segundos para que aparezca el elemento")
+            self.log.info("Esperando a :: " +str(timeout)+ " :: segundos para que aparezca el elemento")
             wait = WebDriverWait(self.driver, 10, poll_frequency=1,
                                  ignored_exceptions=[NoSuchElementException, ElementNotVisibleException,
                                                      ElementNotSelectableException])
             element = wait.until(EC.element_to_be_clickable((byType, locator)))
             element.click()
-            print("Elemento aparecio en la web")
+            self.log.info("Elemento aparecio en la web")
         except:
-            print("No aparecio el elemento")
+            self.log.info("No aparecio el elemento")
             print_stack()
         return element
