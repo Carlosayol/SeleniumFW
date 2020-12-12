4f08045ced2df6ff95c56b427ad08db84ab6fbb4
carlosayol <Carlosaolar@gmail.com>
2020-12-12 13:58:44 -0500

    preparing new classes

---
+++ pages/courses/register_courses_page.py
@@ -0,0 +1,10 @@
+# Esta clase hereda base_page
+# Tener Locators para los diferentes cosas que vamos a escribir,
+#   textbox, clicks y demas
+# Escribir metodos que hagan las acciones
+# Ejm: Seleccionar curso, entrar datos de la tarjeta de credito,
+#   click en enrroll, metodos por separado para entrar los datos de las tarjetas,
+#   macro metodos que llamen metodos mas pequeños
+#   (como enroll course haga el scroll, click y entrar datos)
+#   (verificar el error del mensaje mostrado (el mensaje se demora un poco en aparecer))
+# Crear una test clas
---
+++ tests/courses/register_courses_test.py
@@ -0,0 +1,8 @@
+# Hacer cosas similares a lo que esta en la otra clase de tests
+# Usar fixtures
+# Crear 1 metodo que haga:
+#       ingresar nombre del curso
+#       seleccionarlo
+#       entrar al curso y aplicar al curso
+#       verificar el mensaje de error
+#       test status.markfinal
